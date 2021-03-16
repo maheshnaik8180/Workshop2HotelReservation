5c5d18a07869fe37dfa5bcbeb897b02603001c4a
GENIUS <naikmahesh.naik@gmail.com>
2021-03-17 00:09:11 +0530

    find cheapest hotel

--- src/test/java/workshop2/HotelReservationTest.java
+++ src/test/java/workshop2/HotelReservationTest.java
@@ -15,4 +15,20 @@ public class HotelReservationTest {
         List<Hotel> hotelList = hotelReservationService.getHotels();
         Assert.assertTrue(hotelList.contains(hotel));
     }
+
+    @Test
+    public void given3Hotels_whenInvokeFindCheapestHotel_shouldBeAbleToReturnCheapestHotel() {
+   HotelReservationService hotelReservationService = new HotelReservationService();
+   Hotel Lakewood = new Hotel("Lakewood",110);
+   Hotel Bridgewood = new Hotel("Bridgewood",160);
+   Hotel Ridgewood = new Hotel("Ridgewood",220);
+   hotelReservationService.addHotel(Lakewood);
+   hotelReservationService.addHotel(Bridgewood);
+   hotelReservationService.addHotel(Ridgewood);
+   int countDays = hotelReservationService.countDays("2020-09-10","2020-09-12");
+   System.out.println("Total count days : " +countDays);
+   Hotel result = hotelReservationService.findCheapestHotel();
+   Assert.assertTrue(hotelReservationService.hotelList.contains(result));
+
+    }
 }
